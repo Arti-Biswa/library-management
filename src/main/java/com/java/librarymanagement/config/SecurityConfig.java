48c4320c11ac70a342db1a87af35a938d77d20fe
Arti-Biswa <152138927+Arti-Biswa@users.noreply.github.com>
2024-12-07 13:10:40 +0600

    LMS-09:Implemented cors for backend (#17)

--- src/main/java/com/java/librarymanagement/config/SecurityConfig.java
+++ src/main/java/com/java/librarymanagement/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.java.librarymanagement.config;
 
-
 import com.java.librarymanagement.auth.helper.UserInfoService;
 import com.java.librarymanagement.filter.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
