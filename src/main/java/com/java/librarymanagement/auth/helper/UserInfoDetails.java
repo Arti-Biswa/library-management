9957ccc67ad6c1a5b7fcd436e88eb436bf2ea3ef
Arti-Biswa <152138927+Arti-Biswa@users.noreply.github.com>
2024-12-10 15:49:20 +0600

    LMS-10:Implemented refresh token endpoints (#18)
    * LMS-09:Implemented cors for backend
    
    * LMS-10:Implemented refresh token endpoints

--- src/main/java/com/java/librarymanagement/auth/helper/UserInfoDetails.java
+++ src/main/java/com/java/librarymanagement/auth/helper/UserInfoDetails.java
@@ -1,6 +1,5 @@
 package com.java.librarymanagement.auth.helper;
 
-
 import com.java.librarymanagement.users.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
