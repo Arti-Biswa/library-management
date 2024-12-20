f826f42ab9ab05d9dbb8cc59873725a661923062
Arti-Biswa <aratibiswa481@gmail.com>
2024-12-20 17:37:08 +0600

    LMS-19:Implemented update API for the book entity

--- src/main/java/com/java/librarymanagement/config/SecurityConfig.java
+++ src/main/java/com/java/librarymanagement/config/SecurityConfig.java
@@ -43,9 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login", "/api/v1/auth/sign-up","/api/v1/books/add").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/users").hasAuthority("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/users").hasAuthority("ADMIN")
-
-         .requestMatchers(HttpMethod.PATCH, "/api/v1/users/**").hasAnyAuthority("ADMIN", "USER")
-
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/users/**").hasAnyAuthority("ADMIN", "USER")
 
                         .anyRequest().authenticated() // Protect all other endpoints
                 )
