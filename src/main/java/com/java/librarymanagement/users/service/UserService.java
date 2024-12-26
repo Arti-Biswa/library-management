1588a4ff7b02484db3def098b68276b91b9836a1
tsheylha <163959016+tsheylha@users.noreply.github.com>
2024-12-26 16:16:39 +0600

    lMS-25:Implemente add Api, get API and update API for student entity

--- src/main/java/com/java/librarymanagement/users/service/UserService.java
+++ src/main/java/com/java/librarymanagement/users/service/UserService.java
@@ -1,10 +1,9 @@
-
 package com.java.librarymanagement.users.service;
 
 
-import com.company.coursemanager.utils.IGenericCrudService;
 import com.java.librarymanagement.users.model.User;
 import com.java.librarymanagement.users.model.UserDTO;
+import com.java.librarymanagement.utils.IGenericCrudService;
 
 public interface UserService extends IGenericCrudService<User, UserDTO> {
 
