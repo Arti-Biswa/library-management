ddaec314d1f9dac8b6a82088ab73ab627d34d6a3
Arti-Biswa <152138927+Arti-Biswa@users.noreply.github.com>
2024-12-27 12:58:11 +0600

    LMS-26:Implemented swagger for API documentation (#36)

--- src/main/java/com/java/librarymanagement/student/controller/StudentController.java
+++ src/main/java/com/java/librarymanagement/student/controller/StudentController.java
@@ -7,6 +7,7 @@ import com.java.librarymanagement.student.repository.StudentRepository;
 import com.java.librarymanagement.student.service.StudentServiceImpl;
 import com.java.librarymanagement.utils.RestHelper;
 import com.java.librarymanagement.utils.RestResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Tag(name = "Student Management")
 @RequestMapping("/api/v1/student")
 public class StudentController {
 
